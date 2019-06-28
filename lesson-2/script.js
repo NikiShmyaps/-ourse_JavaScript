e4b763fbb1bf807e489c450be478a59e971ded10
Nikita <mister.nikneim2013@yandex.ru>
2019-06-28 16:15:11 +0300

    Урок №2

---
+++ lesson-2/script.js
@@ -0,0 +1,21 @@
+let money = 60000;
+let income = "Фриланс";
+let addExpenses = "Какие-то, допольнительные, расходы";
+let deposit = true;
+let mission = 2000000;
+let period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(income.length);
+
+console.log("Период " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей");
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(", "));
+
+let budgetDay = money;
+console.log(budgetDay / 30);
+console.log(budgetDay % 30);

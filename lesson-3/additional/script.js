5611b68c4a3d9c6e391da8cd749591042da42131
Nikita <mister.nikneim2013@yandex.ru>
2019-07-01 19:08:20 +0300

    Правки в обязательном и добавил усложненное

--- lesson-3/additional/script.js
+++ lesson-3/additional/script.js
@@ -1,3 +1,31 @@
 'use strict';
 
-let money = promt('Ваш месячный доход?');
+let lang = prompt("Укажите значение 'ru' или 'en'");
+console.log('lang: ', lang);
+if (lang == "ru") {
+    console.log("Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
+}
+else if (lang == "en") {
+    console.log("Monday, tuesday, wednesday, thursday, friday, saturday, sunday");
+}
+
+switch (lang){
+    case "ru":
+        console.log("Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
+        break;
+    case "en":
+        console.log("Monday, tuesday, wednesday, thursday, friday, saturday, sunday");
+        break;
+}
+
+let result = lang == "ru" ? "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье" : "Monday, tuesday, wednesday, thursday, friday, saturday, sunday";
+console.log(result);
+
+//артем директор 
+//максим преподователь
+//другое студент
+
+let namePerson = prompt("Укажите 'Артем', 'Максим' или другое имя");
+let nameResult = namePerson == "Артем" ? "Директор" : namePerson == "Максим" ? "Преподователь" : "Студент";
+console.log(nameResult);
+
--- lesson-3/obligatory/script.js
+++ lesson-3/obligatory/script.js
@@ -1,22 +1,31 @@
 'use strict';
 
-let money = prompt('Ваш месячный доход?');
-console.log(typeof money);
+let money = +prompt('Ваш месячный доход?');
+
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую:');
 addExpenses.split(", ");
-console.log(typeof addExpenses);
-let deposit = confirm('Есть ли у вас депозит в банке?');
-console.log(typeof deposit);
-let expensesOne = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
-    sumOne = prompt('Во сколько это обойдется?'),
-    expensesTwo = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
-    sumTwo = prompt('Во сколько это обойдется?');
 
-let budgetMonth = Number(money) - (Number(sumOne) + Number(sumTwo));
-console.log('budgetMonth: ', budgetMonth);
-let mission = prompt('Ваша цель?');
-console.log(Math.ceil((Number(mission) / budgetMonth)));
+let deposit = confirm('Есть ли у вас депозит в банке?');
+
+let income = confirm('Есть ли у вас дополнительный доход');
+
+let expensesOne = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
+    sumOne = +prompt('Во сколько это обойдется?'),
+    expensesTwo = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
+    sumTwo = +prompt('Во сколько это обойдется?');
+
+let budgetMonth = money - sumOne + sumTwo;
+
+let mission = +prompt('Ваша цель?');
+
 let budgetDay = budgetMonth / 30;
+
+console.log(typeof money);
+console.log(addExpenses);
+console.log(typeof deposit);
+console.log(typeof income);
+console.log('budgetMonth: ', budgetMonth);
+console.log(Math.ceil((mission / budgetMonth)));
 console.log(Math.floor(budgetDay));
 
 if (budgetDay >= 800) {

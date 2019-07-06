46930012b397e618df058adbcbf85689b8a2499d
Nikita <mister.nikneim2013@yandex.ru>
2019-07-07 00:14:29 +0300

    упростил

--- lesson-6/additional/script.js
+++ lesson-6/additional/script.js
@@ -1,15 +1,13 @@
-let week = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
-let date = new Date(2019, 6, 6);
-let dayWeek = date.getDay();
+let week = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+    date = new Date(2019, 6, 6),
+    dayWeek = date.getDay();
 // document.write("Сегодня " + week[dayWeek] + "<br>");
 for (let i = 0; i < week.length; i++) {
-    if (i == 0) {
-        document.write(week[i].italics() + "<br>");
+    if (i < 5) {
+        document.write(week[i] + "<br>");
     } else if (i == dayWeek) {
         document.write(week[i].bold() + "<br>");
-    } else if (i == 6) {
+    } else if (i >= 5) {
         document.write(week[i].italics() + "<br>");
-    } else {
-        document.write(week[i] + "<br>");
     }
 }

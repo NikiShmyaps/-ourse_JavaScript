5bfe7ebe9d503eb9652fec9c260fbd376c3f624c
Nikita <mister.nikneim2013@yandex.ru>
2019-07-06 20:49:03 +0300

    Усложненное

--- lesson-6/additional/script.js
+++ lesson-6/additional/script.js
@@ -1,20 +1,15 @@
-let arr = ['11', '22', '33', '44', '55', '66', '77'];
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i].charAt(0) == 2 || arr[i].charAt(0) == 4) {
-        console.log(arr[i]);
-    }
-}
-
-
-let arrTwo = [];
-metka: for (let i = 0; i <= 100; i++) {
-    arrTwo[i] = i;
-    if (i > 1) {
-        for (let j = 2; j < i; j++) {
-            if (i % j == 0) {
-                continue metka;
-            }
-        }
-        console.log(arrTwo[i] + " Делители этого числа: 1 и " + i);
+let week = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+let date = new Date(2019, 6, 6);
+let dayWeek = date.getDay();
+// document.write("Сегодня " + week[dayWeek] + "<br>");
+for (let i = 0; i < week.length; i++) {
+    if (i == 0) {
+        document.write(week[i].italics() + "<br>");
+    } else if (i == 6) {
+        document.write(week[i].italics() + "<br>");
+    } else if (i == dayWeek) {
+        document.write(week[i].bold() + "<br>");
+    } else {
+        document.write(week[i] + "<br>");
     }
 }

0f93d5777d2f0295538720e1b8c834e644b52802
Nikita <mister.nikneim2013@yandex.ru>
2019-07-10 00:07:27 +0300

    Усложненное

---
+++ lesson-7/additional/script.js
@@ -0,0 +1,36 @@
+let dataTime = new Date(),
+    hours = dataTime.getHours(),
+    minutes = dataTime.getMinutes(),
+    seconds = dataTime.getSeconds(),
+    d = dataTime.getDay(),
+    date = dataTime.getDate(),
+    year = dataTime.getFullYear(),
+    month = (dataTime.getMonth() + 1);
+
+
+function showData(dataTime) {
+    hours = dataTime.getHours().toString();
+    minutes = dataTime.getMinutes().toString();
+    seconds = dataTime.getSeconds().toString();
+    date = dataTime.getDate().toString();
+    month = (dataTime.getMonth() + 1).toString();
+    year = dataTime.getFullYear();
+
+    if (hours.length < 2) {
+        hours = "0" + hours;
+    }
+    if (minutes.length < 2) {
+        minutes = "0" + minutes;
+    }
+    if (seconds.length < 2) {
+        seconds = "0" + seconds;
+    }
+    if (date.length < 2) {
+        date = "0" + date;
+    }
+    if (month.length < 2) {
+        month = "0" + month;
+    }
+    console.log(hours + ":" + minutes + ":" + seconds + " " + date + "." + month + "." + year);
+}
+showData(dataTime);

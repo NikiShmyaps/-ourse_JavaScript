1cda6051c5f8771b9fbdcbe5e0ea9282cd1c7ce1
Nikita <mister.nikneim2013@yandex.ru>
2019-07-21 14:22:41 +0300

    Не работатет

--- lesson-12/obligatory/DOP/script.js
+++ lesson-12/obligatory/DOP/script.js
@@ -1,6 +1,14 @@
 'use strict';
 
-let year = new Date().getFullYear(),
+let hello = document.querySelector('#hello'),
+    presently = document.querySelector('#presently'),
+    timerHours = document.querySelector('#hours'),
+    timerMinutes = document.querySelector('#minutes'),
+    timerSeconds = document.querySelector('#seconds'),
+    ampm = document.querySelector('#ampm'),
+    days = document.querySelector('#days'),
+
+    year = new Date().getFullYear(),
     month = new Date().getMonth(),
     date = new Date().getDay(),
     hours = new Date().getHours(),
@@ -9,65 +17,60 @@ let year = new Date().getFullYear(),
     week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
     timesDay = '';
 
-if(hours < 12){
-    timesDay = 'Доброе утро';
-}else if(hours < 6 || hours > 20){
-    timesDay = 'Доброй ночи';
-}else{
-    timesDay = 'Добрый день';
-}
 
-function presently (){
-    let text = 'Сегодня: ',
-        presently = week[date],
-        newText = text + presently;
-        creatBlock(newText);
-}
-
-if(hours.toString().length < 2){
-    hours = '0' + hours;
-}
-if(minutes.toString().length < 2){
-    minutes = '0' + minutes;
-}
-if(seconds.toString().length < 2){
-    seconds = '0' + seconds;
-}
-function time(){
-    let am = 'AM',
-        pm = 'PM',
-        time = '',
-        text = 'Текущее время: ',
-        newText = '';
-    if(hours >= 12){
-        time = (hours % 12) + ':' + minutes + ':' + seconds + " " + am;
+function getWelcom(){
+    if(hours < 12){
+        timesDay = 'Доброе утро';
+    }else if(hours < 6 || hours > 20){
+        timesDay = 'Доброй ночи';
     }else{
-        time = (hours % 12) + ':' + minutes + ':' + seconds + " " + pm;
+        timesDay = 'Добрый день';
     }
-    newText = text + time;
-    creatBlock(newText);
+
+    hello.textContent = timesDay;
 }
 
-
-
-function newYear(){
-    let deadline = new Date('1 Jan 2020'),
-        days = Math.floor((deadline.getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000)),
-        text = 'До нового года осталось: ',
-        newText = text + days + ' дня';
-    creatBlock(newText);
+function presentlyText(){
+    presently.textContent = week[date];
 }
 
-creatBlock(timesDay);
-presently();
-time();
-newYear();
+function time(){
+    if(hours.toString().length < 2){
+        timerHours.textContent = '0' + (hours % 12);
+    }else{
+        timerHours.textContent = hours % 12;
+    }
+    if(minutes.toString().length < 2){
+        timerMinutes.textContent = '0' + minutes;
+    }else{
+        timerMinutes.textContent = minutes;
+    }
+    if(seconds.toString().length < 2){
+        timerSeconds.textContent = '0' + seconds;
+    }else{
+        timerSeconds.textContent = seconds;
+    }
 
-function creatBlock(item){
-    var block;
-    block = document.createElement('div');
-    block.innerHTML = item;
-    document.body.appendChild(block);
+    if(hours >= 12){
+        ampm.textContent = 'AM';
+    }else{
+        ampm.textContent = 'PM';
+    }
 }
 
+function newYear(deadline){
+    let dateLast = new Date(deadline).getTime(),
+        dayOfNY = Math.floor((dateLast - new Date().getTime()) / (24 * 60 * 60 * 1000));
+    days.textContent = dayOfNY;
+}
 
+function start(){
+    
+}
+
+setInterval(() => {
+    getWelcom();
+    presentlyText();
+    time();
+    newYear('1 Jan 2020');
+}, 1000);

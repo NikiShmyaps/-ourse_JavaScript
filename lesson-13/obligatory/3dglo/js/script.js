6f4de7eb648a6fa4cbafaf25af54f4203e1fdbb8
nikita <mister.nikneim2013@yandex.ru>
2019-07-23 17:17:05 +0400

    Обязательное

--- lesson-13/obligatory/3dglo/js/script.js
+++ lesson-13/obligatory/3dglo/js/script.js
@@ -49,4 +49,66 @@ window.addEventListener('DOMContentLoaded', function(){
         updateClock();
     }
     countTimer('21 july 2019');
+
+    // menu 
+    const toggleMenu = () =>{ 
+        const btnMenu = document.querySelector('.menu'), 
+            menu = document.querySelector('menu'), 
+            closeBtn = document.querySelector('.close-btn'), 
+            menuItems = menu.querySelectorAll('ul>li'); 
+        
+        const handlerMenu = () => { 
+            menu.classList.toggle('active-menu'); 
+        }; 
+        btnMenu.addEventListener('click', handlerMenu); 
+        closeBtn.addEventListener('click', handlerMenu); 
+        
+        // for(let i = 0; i < menuItems.length; i++){ 
+        // menuItems[i].addEventListener('click',handlerMenu); 
+        // } 
+        menuItems.forEach((elem)=> elem.addEventListener('click', handlerMenu)); 
+    }; 
+
+    toggleMenu(); 
+
+    // popup 
+
+    const togglePopUp = () => { 
+        const popup = document.querySelector('.popup'), 
+            popupBtn = document.querySelectorAll('.popup-btn'), 
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content');
+
+        popupBtn.forEach((elem) => { 
+            elem.addEventListener('click', () => { 
+                let clientWidth = document.documentElement.clientWidth;
+                if(clientWidth > 768){
+                    popup.style.display = 'block';
+                    
+                    let statrAnimate = Date.now(), //Время начала
+                        timerAnimate = setInterval(function(){
+                            let timePassed = Date.now() - statrAnimate; // Пройденное время
+                            if(timePassed >= 1000){
+                                clearInterval(timerAnimate);
+                                return;
+                            } 
+                            playAnimate(timePassed);
+                        }, 10);
+                    const playAnimate = (timePassed) => {
+                        popupContent.style.top = timePassed / 15 + 'px';
+
+                    };
+                }else{
+                    popup.style.display = 'block';
+                }
+
+                }); 
+        }); 
+            
+        popupClose.addEventListener('click', () => { 
+            popup.style.display = 'none'; 
+        }); 
+    }; 
+        
+    togglePopUp();
 });

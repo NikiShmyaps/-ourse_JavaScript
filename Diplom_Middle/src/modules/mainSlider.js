4099193cfc14d9e643d591283f07b9e4c2ef44aa
Nikita <mister.nikneim2013@yandex.ru>
2019-08-13 14:14:56 +0300

    Глайвный слайдер

--- Diplom_Middle/src/modules/mainSlider.js
+++ Diplom_Middle/src/modules/mainSlider.js
@@ -1,7 +1,64 @@
-//slider(`.main-slider`, `.slide`, true, false, 1, 5000);
-//slider(`.gallery-bg`, `.slide`, true, true, 1, 5000);
-
 const mainSlider = () => {
+  const slide = document.querySelectorAll('.main-slider>.slide'),
+    slider = document.querySelector('.main-slider');
+
+
+    const createDots = () => {
+      const ul = document.createElement('ul');
+      ul.setAttribute('class', 'slider-dots');
+      slider.appendChild(ul);
+      for (let i = 0; i < slide.length; i++) {
+        const ul = slider.querySelector('.slider-dots'),
+          li = document.createElement('li');
+        if (i == 0) {
+          li.setAttribute('class', 'main-dot main-dot-active');
+          ul.appendChild(li);
+        } else {
+          li.setAttribute('class', 'main-dot');
+          ul.appendChild(li);
+        }
+      }
+    };
+
+    createDots();
+    const dot = slider.querySelectorAll('.main-dot');
+
+    let currentSlide = 0;
+
+    const autoPlaySlide = () => {
+      slide[currentSlide].classList.remove('slide-active');
+      dot[currentSlide].classList.remove('dot-active');
+      currentSlide++;
+      if(currentSlide >= slide.length){
+        currentSlide = 0;
+      }
+      slide[currentSlide].classList.add('slide-active');
+      dot[currentSlide].classList.add('dot-active');
+    };
+
+    const startSlide = () => {
+      setInterval(autoPlaySlide, 3000);
+    };
+
+    slider.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      let target = event.target;
+
+      slide[currentSlide].classList.remove('slide-active');
+      dot[currentSlide].classList.remove('dot-active');
+      if(target.matches('.main-dot')){
+        dot.forEach((elem, index) => {
+          if (elem === target){
+            currentSlide = index;
+          }
+        });
+      }
+      slide[currentSlide].classList.add('slide-active');
+      dot[currentSlide].classList.add('dot-active');
+    });
+
+    startSlide();
     
   };
 

0e69903969e8f8ef20579ad382558412ccc2c4d7
Nikita <mister.nikneim2013@yandex.ru>
2019-08-14 14:10:32 +0300

    У главного слайда, на время, убраны точки пагинации

--- Diplom_Middle/src/modules/gallerySlider.js
+++ Diplom_Middle/src/modules/gallerySlider.js
@@ -2,36 +2,29 @@ const gallerySlider = () => {
     const slide = document.querySelectorAll('.gallery-slider>.slide'),
         slider = document.querySelector('.gallery-slider');
         
-    const createDots = () => {
-        const ul = document.createElement('ul');
-        ul.setAttribute('class', 'slider-dots');
-        slider.appendChild(ul);
-        for (let i = 0; i < slide.length; i++) {
-            const ul = slider.querySelector('.slider-dots'),
-                li = document.createElement('li');
-            if (i == 0) {
-                li.setAttribute('class', 'main-dot main-dot-active');
-                ul.appendChild(li);
-            } else {
-                li.setAttribute('class', 'main-dot');
-                ul.appendChild(li);
-            }
+    const ul = document.createElement('ul');
+    ul.setAttribute('class', 'slider-dots');
+    slider.appendChild(ul);
+    for (let i = 0; i < slide.length; i++) {
+        const ul = slider.querySelector('.slider-dots'),
+            li = document.createElement('li');
+        if (i == 0) {
+            li.setAttribute('class', 'main-dot dot-active');
+            ul.appendChild(li);
+        } else {
+            li.setAttribute('class', 'main-dot');
+            ul.appendChild(li);
         }
-    };
+    }
 
-    const createArrows = () => {
-        const btnPrev = document.createElement(`a`),
-        btnNext = document.createElement(`a`);
-        btnPrev.setAttribute("class", "slider-btn prev");
-        btnPrev.setAttribute("id", "arrow-left");
-        btnNext.setAttribute("class", "slider-btn next");
-        btnNext.setAttribute("id", "arrow-right");
-        slider.appendChild(btnPrev);
-        slider.appendChild(btnNext);
-    };
-      
-    createArrows();
-    createDots();
+    const btnPrev = document.createElement(`a`),
+    btnNext = document.createElement(`a`);
+    btnPrev.setAttribute("class", "slider-btn prev");
+    btnPrev.setAttribute("id", "arrow-left");
+    btnNext.setAttribute("class", "slider-btn next");
+    btnNext.setAttribute("id", "arrow-right");
+    slider.appendChild(btnPrev);
+    slider.appendChild(btnNext);
 
     const dot = slider.querySelectorAll('.main-dot');
       
--- Diplom_Middle/src/modules/mainSlider.js
+++ Diplom_Middle/src/modules/mainSlider.js
@@ -3,60 +3,61 @@ const mainSlider = () => {
     slider = document.querySelector('.main-slider');
 
 
-    const createDots = () => {
-      const ul = document.createElement('ul');
-      ul.setAttribute('class', 'slider-dots');
-      slider.appendChild(ul);
-      for (let i = 0; i < slide.length; i++) {
-        const ul = slider.querySelector('.slider-dots'),
-          li = document.createElement('li');
-        if (i == 0) {
-          li.setAttribute('class', 'main-dot main-dot-active');
-          ul.appendChild(li);
-        } else {
-          li.setAttribute('class', 'main-dot');
-          ul.appendChild(li);
-        }
-      }
-    };
+    // const ul = document.createElement('ul');
+    // ul.setAttribute('class', 'slider-dots');
+    // slider.appendChild(ul);
+    // for (let i = 0; i < slide.length; i++) {
+    //   const ul = slider.querySelector('.slider-dots'),
+    //     li = document.createElement('li');
+    //   if (i == 0) {
+    //     li.setAttribute('class', 'main-dot dot-active');
+    //     ul.appendChild(li);
+    //   } else {
+    //     li.setAttribute('class', 'main-dot');
+    //     ul.appendChild(li);
+    //   }
+    // }
 
-    createDots();
-    const dot = slider.querySelectorAll('.main-dot');
+    // const dot = slider.querySelectorAll('.main-dot');
 
     let currentSlide = 0;
 
     const autoPlaySlide = () => {
       slide[currentSlide].classList.remove('slide-active');
-      dot[currentSlide].classList.remove('dot-active');
+      // dot[currentSlide].classList.remove('dot-active');
       currentSlide++;
       if(currentSlide >= slide.length){
         currentSlide = 0;
       }
       slide[currentSlide].classList.add('slide-active');
-      dot[currentSlide].classList.add('dot-active');
+      // dot[currentSlide].classList.add('dot-active');
     };
 
     const startSlide = () => {
-      setInterval(autoPlaySlide, 3000);
+      setInterval(autoPlaySlide, 5000);
     };
 
-    slider.addEventListener('click', (event) => {
-      event.preventDefault();
+    // slider.addEventListener('click', (event) => {
+    //   event.preventDefault();
 
-      let target = event.target;
+    //   let target = event.target;
 
-      slide[currentSlide].classList.remove('slide-active');
-      dot[currentSlide].classList.remove('dot-active');
-      if(target.matches('.main-dot')){
-        dot.forEach((elem, index) => {
-          if (elem === target){
-            currentSlide = index;
-          }
-        });
-      }
-      slide[currentSlide].classList.add('slide-active');
-      dot[currentSlide].classList.add('dot-active');
-    });
+    //   if(!target.matches('.main-dot')){
+    //     return;
+    //   }
+
+    //   slide[currentSlide].classList.remove('slide-active');
+    //   dot[currentSlide].classList.remove('dot-active');
+    //   if(target.matches('.main-dot')){
+    //     dot.forEach((elem, index) => {
+    //       if (elem === target){
+    //         currentSlide = index;
+    //       }
+    //     });
+    //   }
+    //   slide[currentSlide].classList.add('slide-active');
+    //   dot[currentSlide].classList.add('dot-active');
+    // });
 
     startSlide();
     
